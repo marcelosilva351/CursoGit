c3d174db344ff4decb7d4c27934f15818204ee4b
Marcelo <marcelosilva23099@gmail.com>
2021-06-18 14:59:21 -0300

    Acrescentado a linha commit sem msg

--- GitTeste/Program.cs
+++ GitTeste/Program.cs
@@ -8,6 +8,7 @@ namespace GitTeste
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
+            Console.WriteLine("Comit sem msg");
         }
     }
 }
